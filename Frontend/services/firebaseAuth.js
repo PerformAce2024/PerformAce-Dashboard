234b51def5110b02ceb074ff5b60bc9a30161d7e
Harsh06-afk <hvs8380@gmail.com>
2024-10-22 13:02:36 +0530

    Added: firebaseAuth

---
+++ Frontend/services/firebaseAuth.js
@@ -0,0 +1,13 @@
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js";
+  const firebaseConfig = {
+    apiKey: "AIzaSyCIfu_V5cAaclMpDt46C9WeVF-Qyn7yxEY",
+    authDomain: "performacemedia-dashboard.firebaseapp.com",
+    projectId: "performacemedia-dashboard",
+    storageBucket: "performacemedia-dashboard.appspot.com",
+    messagingSenderId: "861288596980",
+    appId: "1:861288596980:web:87d7fd5978d03f7d5525a1"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
