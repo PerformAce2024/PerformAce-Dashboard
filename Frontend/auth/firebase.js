1c1d76164cbe96c182bfe69ff84ff5ac3282f353
Harsh06-afk <hvs8380@gmail.com>
2024-10-22 17:36:03 +0530

    Update : auth

--- Frontend/auth/firebase.js
+++ Frontend/auth/firebase.js
@@ -1,3 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCQvJe0-NL3eD028sGVF5ULsJ65Vmz7TeQ",
+    authDomain: "performacemedia-dashbaord.firebaseapp.com",
+    projectId: "performacemedia-dashbaord",
+    storageBucket: "performacemedia-dashbaord.appspot.com",
+    messagingSenderId: "534541753220",
+    appId: "1:534541753220:web:102e385b339255abe69bd1",
+    measurementId: "G-W1NQY9X937"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 document.getElementById('loginButton').addEventListener('click', () => {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
@@ -28,13 +48,13 @@ document.getElementById('loginButton').addEventListener('click', () => {
       .then((data) => {
         // Redirect based on user role
         if (data.role === 'admin') {
-          window.location.href = 'admin-dashboard.html'; // Redirect to admin dashboard
+            window.location.href = '../admin/admin_homepage.html'; // Redirect to admin dashboard
         } else if (data.role === 'client') {
-          window.location.href = 'client-dashboard.html'; // Redirect to client dashboard
+            window.location.href = '../client/client_dashboard.html'; // Redirect to client dashboard
         } else {
-          alert('Unauthorized access');
+            alert('Unauthorized access');
         }
-      })
+    })
       .catch((error) => {
         console.error('Login error:', error);
         alert('Login failed: Incorrect email or password'); // Display error message on login failure
