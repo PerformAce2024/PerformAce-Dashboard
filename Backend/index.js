5dc5fadf69136ce0f6196de9a95d8b0c0694e52c
PerformAce2024 <srishti@performacemedia.com>
2024-12-21 14:20:00 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -35,8 +35,8 @@ const app = express();
 // Configure CORS
 app.use(cors({
   origin: '*', // Allow only this domain
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  // credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'],
   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
 }));
 
