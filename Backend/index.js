9d988c2ae96a2615c18651f9e0fdf88354f19c75
Prakhar <prakharb56@gmail.com>
2025-03-03 17:16:28 +0530

    fix: cors again

--- Backend/index.js
+++ Backend/index.js
@@ -31,26 +31,9 @@ const __dirname = dirname(__filename);
 // Configure CORS
 app.use(
   cors({
-    // origin: "https://insights.performacemedia.com", // Allow only this domain
-    // credentials: true,
-    // methods: ["GET", "POST", "PUT", "DELETE"],
-    // allowedHeaders: [
-    //   "Origin",
-    //   "X-Requested-With",
-    //   "Content-Type",
-    //   "Accept",
-    //   "Authorization",
-    // ],
-    origin: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: [
-      "Origin",
-      "X-Requested-With",
-      "Content-Type",
-      "Accept",
-      "Authorization",
-    ],
+    origin: "insights.performacemedia.com", // Allow only this domain
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 
