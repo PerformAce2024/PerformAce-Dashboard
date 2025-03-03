605877f76ddb77658f61b81c74b12e2eb191e0f5
Prakhar <prakharb56@gmail.com>
2025-03-03 17:03:28 +0530

    fix: cors

--- Backend/index.js
+++ Backend/index.js
@@ -41,7 +41,7 @@ app.use(
     //   "Accept",
     //   "Authorization",
     // ],
-    origin: "*",
+    origin: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: [
       "Origin",
@@ -50,6 +50,7 @@ app.use(
       "Accept",
       "Authorization",
     ],
+    credentials: true,
   })
 );
 
