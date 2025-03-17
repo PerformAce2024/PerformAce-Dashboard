57487e09be0f7b2e2f2a3f61f915acb294e27ab5
Prakhar <prakharb56@gmail.com>
2025-03-17 16:57:01 +0530

    frontend changes

--- Frontend/helper/config.js
+++ Frontend/helper/config.js
@@ -1,6 +1,6 @@
 const config = {
-  // BASE_URL: "https://backend-api.performacemedia.com:8000", // Change this when deploying
-  BASE_URL: "http://localhost:8000", // Change this when deploying
+  BASE_URL: "https://backend-api.performacemedia.com:8000", // Change this when deploying
+  // BASE_URL: "http://localhost:8000", // Change this when deploying
 };
 
 export default config;
